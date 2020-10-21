ab67c9e5d2a5f5357cfdaf9205f0f52c5ece3b2a
STJ0215 <stj960215@gmail.com>
2020-10-21 15:28:48 +0900

    게시물 리스팅 기초 1

---
+++ src/main/java/com/sbs/webp/lolHi/controller/usr/ArticleController.java
@@ -0,0 +1,27 @@
+package com.sbs.webp.lolHi.controller.usr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbs.webp.lolHi.dto.Article;
+
+
+@Controller
+public class ArticleController {
+	@RequestMapping("/usr/article/list")
+	@ResponseBody
+	List<Article> showList() {
+		List<Article> articles = new ArrayList<>();
+		Article article1 = new Article(1, "2020-10-17 15:00:00", "2020-10-17 18:00:00", "제목1", "내용1");
+		Article article2 = new Article(2, "2020-10-18 15:00:00", "2020-10-18 18:00:00", "제목2", "내용2");
+
+		articles.add(article1);
+		articles.add(article2);
+
+		return articles;
+	}
+}
