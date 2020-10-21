7ff9eb386bbf52007a1e61c2ba3d26eda679a615
STJ0215 <stj960215@gmail.com>
2020-10-21 15:17:37 +0900

    프로젝트 생성

---
+++ src/main/java/com/sbs/webp/lolHi/LolHiApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.webp.lolHi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolHiApplication.class, args);
+	}
+
+}
