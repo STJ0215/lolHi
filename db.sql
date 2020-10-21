e66954a41ad83d38d0e87db8aeac5cca0e2e096a
STJ0215 <stj960215@gmail.com>
2020-10-22 03:09:53 +0900

    게시물 리스트에서 page 파라미터에 의해 페이지 구분

--- db.sql
+++ db.sql
@@ -25,4 +25,184 @@ updateDate = NOW(),
 title = '제목2',
 `body` = '내용2';
 
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목4',
+`body` = '내용4';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목5',
+`body` = '내용5';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목6',
+`body` = '내용6';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목7',
+`body` = '내용7';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목8',
+`body` = '내용8';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목9',
+`body` = '내용9';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목10',
+`body` = '내용10';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목11',
+`body` = '내용11';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목12',
+`body` = '내용12';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목13',
+`body` = '내용13';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목14',
+`body` = '내용14';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목15',
+`body` = '내용15';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목16',
+`body` = '내용16';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목17',
+`body` = '내용17';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목18',
+`body` = '내용18';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목19',
+`body` = '내용19';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목20',
+`body` = '내용20';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목21',
+`body` = '내용21';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목22',
+`body` = '내용22';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목23',
+`body` = '내용23';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목24',
+`body` = '내용24';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목25',
+`body` = '내용25';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목26',
+`body` = '내용26';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목27',
+`body` = '내용27';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목28',
+`body` = '내용28';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목29',
+`body` = '내용29';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목30',
+`body` = '내용30';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목31',
+`body` = '내용31';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목32',
+`body` = '내용32';
+
 SELECT * FROM article;
--- src/main/java/com/sbs/webp/lolHi/util/Util.java
+++ src/main/java/com/sbs/webp/lolHi/util/Util.java
@@ -6,6 +6,10 @@ import java.math.BigInteger;
 public class Util {
 
 	public static int getAsInt(Object object) {
+		return getAsInt(object, -1);
+	}
+
+	public static int getAsInt(Object object, int defaultValue) {
 		if (object instanceof BigInteger) {
 			return ((BigInteger) object).intValue();
 		}
@@ -15,7 +19,10 @@ public class Util {
 		else if (object instanceof Long) {
 			return (int)((long) object);
 		}
+		else if (object instanceof String) {
+			return Integer.parseInt((String) object);
+		}
 		
-		return -1;
+		return defaultValue;
 	}
 }
