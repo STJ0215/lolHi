4775a68fd33757d966096a57a186269c0acf9d74
STJ0215 <stj960215@gmail.com>
2020-10-21 15:58:29 +0900

    게시물 리스팅

--- src/main/java/com/sbs/webp/lolHi/service/ArticleService.java
+++ src/main/java/com/sbs/webp/lolHi/service/ArticleService.java
@@ -1,24 +1,20 @@
 package com.sbs.webp.lolHi.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sbs.webp.lolHi.dao.ArticleDao;
 import com.sbs.webp.lolHi.dto.Article;
 
 
 @Service
 public class ArticleService {
+	@Autowired
+	private ArticleDao articleDao;
 
 	public List<Article> getArticles() {
-		List<Article> articles = new ArrayList<>();
-		Article article1 = new Article(1, "2020-10-17 15:00:00", "2020-10-17 18:00:00", "제목1", "내용1");
-		Article article2 = new Article(2, "2020-10-18 15:00:00", "2020-10-18 18:00:00", "제목2", "내용2");
-
-		articles.add(article1);
-		articles.add(article2);
-
-		return articles;
+		return articleDao.getArticles();
 	}
 }
