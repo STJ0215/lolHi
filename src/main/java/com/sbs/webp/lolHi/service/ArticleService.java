4c9dae4251451540804a3e018dd90c404071f98c
STJ0215 <stj960215@gmail.com>
2020-11-11 14:57:48 +0900

    로그아웃 상태에서 게시물 리스트 페이지와 상세 정보가 작동하지 않는 오류 수정

--- src/main/java/com/sbs/webp/lolHi/service/ArticleService.java
+++ src/main/java/com/sbs/webp/lolHi/service/ArticleService.java
@@ -43,8 +43,17 @@ public class ArticleService {
 				article.setExtra(new HashMap<>()); 
 			}
 			
-			boolean actorCanModify = actorMember.getId() == article.getMemberId();
-			boolean actorCanDelete = actorMember.getId() == article.getMemberId();
+			boolean actorCanModify = false;
+			
+			if (actorMember != null) {
+				actorCanModify = actorMember.getId() == article.getMemberId();
+			}
+			
+			boolean actorCanDelete = false;
+			
+			if (actorMember != null) {
+				actorCanDelete = actorMember.getId() == article.getMemberId();
+			}
 
 			article.getExtra().put("actorCanModify", actorCanModify);
 			article.getExtra().put("actorCanDelete", actorCanDelete);
@@ -64,8 +73,17 @@ public class ArticleService {
 			article.setExtra(new HashMap<>());
 		}
 		
-		boolean actorCanModify = actorMember.getId() == article.getMemberId();
-		boolean actorCanDelete = actorMember.getId() == article.getMemberId();
+		boolean actorCanModify = false;
+		
+		if (actorMember != null) {
+			actorCanModify = actorMember.getId() == article.getMemberId();
+		}
+		
+		boolean actorCanDelete = false;
+		
+		if (actorMember != null) {
+			actorCanDelete = actorMember.getId() == article.getMemberId();
+		}
 		
 		article.getExtra().put("actorCanModify", actorCanModify);
 		article.getExtra().put("actorCanDelete", actorCanDelete);
