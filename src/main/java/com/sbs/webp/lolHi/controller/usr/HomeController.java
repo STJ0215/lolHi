074c92d61036411efbd7aa080e09a0231c43347d
STJ0215 <stj960215@gmail.com>
2020-11-18 11:59:26 +0900

    임시 메인 페이지 생성

--- src/main/java/com/sbs/webp/lolHi/controller/usr/HomeController.java
+++ src/main/java/com/sbs/webp/lolHi/controller/usr/HomeController.java
@@ -8,21 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "안녕";
-	}
-
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "개";
-	}
-
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "고양이";
+		return "/usr/home/main";
 	}
 
 	@RequestMapping("/usr/home/plus")
